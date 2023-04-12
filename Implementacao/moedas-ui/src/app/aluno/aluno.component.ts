b0183e7a78f60741836515b1490b3e791558dbaf
gustavoevf <gustavoevf@gmail.com>
2023-04-12 20:29:23 -0300

    criacao projeto angular

---
+++ Implementacao/moedas-ui/src/app/aluno/aluno.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aluno',
+  templateUrl: './aluno.component.html',
+  styleUrls: ['./aluno.component.less']
+})
+export class AlunoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
