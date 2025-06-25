e7d51d272bf4b7f37622b64a47f87ca52dd8a65b
gustavoevf <gustavoevf@gmail.com>
2025-06-25 19:52:33 -0300

    fix: adjusting cors configurations

--- Implementacao/backEnd/moedaestudantil-api/src/main/java/com/api/moedaestudantil/controllers/EmpresaController.java
+++ Implementacao/backEnd/moedaestudantil-api/src/main/java/com/api/moedaestudantil/controllers/EmpresaController.java
@@ -1,8 +1,10 @@
 package com.api.moedaestudantil.controllers;
 
-import com.api.moedaestudantil.dtos.EmpresaDto;
-import com.api.moedaestudantil.models.EmpresaModel;
-import com.api.moedaestudantil.services.EmpresaService;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,14 +12,20 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.Optional;
-import java.util.UUID;
+import com.api.moedaestudantil.dtos.EmpresaDto;
+import com.api.moedaestudantil.models.EmpresaModel;
+import com.api.moedaestudantil.services.EmpresaService;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/empresa")
 public class EmpresaController {
 
